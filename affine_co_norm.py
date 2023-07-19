431a957ccc4402ead43570672f082fb10e676495
chuyan99 <74494017+chuyan99@users.noreply.github.com>
2023-07-19 11:28:05 +0800

    Add files via upload

---
+++ affine_co_norm.py
@@ -0,0 +1,211 @@
+import torch.nn.functional as F
+from torch.nn.modules.module import Module
+from torch.nn.parameter import Parameter
+import torch
+import itertools
+
+
+class _AffineCoNorm(Module):
+
+    def __init__(self, num_features, tem=1, eps=1e-5, momentum=0.1, affine=True, track_running_stats=True):
+        super(_AffineCoNorm, self).__init__()
+        self.num_features = num_features
+        self.eps = eps
+        self.momentum = momentum
+        self.affine = affine
+        self.track_running_stats = track_running_stats
+        # temperature coefficient
+        self.tem = tem
+
+        if self.affine:
+            self.weight = Parameter(torch.Tensor(num_features))
+            self.bias = Parameter(torch.Tensor(num_features))
+            self.weight_target = Parameter(torch.Tensor(num_features))
+            self.bias_target = Parameter(torch.Tensor(num_features))
+        else:
+            self.register_parameter('weight', None)
+            self.register_parameter('bias', None)
+            self.register_parameter('weight_target', None)
+            self.register_parameter('bias_target', None)
+
+        if self.track_running_stats:
+            self.register_buffer('running_mean', torch.zeros(num_features))
+            self.register_buffer('running_var', torch.ones(num_features))
+            self.register_buffer('num_batches_tracked', torch.tensor(0, dtype=torch.long))
+        else:
+            self.register_parameter('running_mean', None)
+            self.register_parameter('running_var', None)
+        self.reset_parameters()
+
+        # fixed source affines
+        self.weight.requires_grad = False
+        self.bias.requires_grad = False
+
+    def reset_parameters(self):
+        if self.track_running_stats:
+            self.running_mean.zero_()
+            self.running_var.fill_(1)
+        if self.affine:
+            self.weight.data.uniform_()
+            self.bias.data.zero_()
+            self.weight_target.data.uniform_()
+            self.bias_target.data.zero_()
+
+    def _check_input_dim(self, input):
+        return NotImplemented
+
+    def _load_from_state_dict_from_pretrained_model(self, state_dict, prefix, metadata, strict, missing_keys, unexpected_keys, error_msgs):
+        local_name_params = itertools.chain(self._parameters.items(), self._buffers.items())
+        local_state = {k: v.data for k, v in local_name_params if v is not None}
+        mode = None
+        for key in state_dict:
+            if 'target' in key:
+                # for continue fine-tuning or inference
+                mode = 'target'
+                break
+            else:
+                # for transferring source parameters to the target model at the beginning of fine-tuning
+                mode = 'source2target'
+        for name, param in local_state.items():
+            key = prefix + name
+            if mode == 'source2target':
+                if 'source' in key or 'target' in key:
+                    key = key[:-7]
+            if key in state_dict:
+                input_param = state_dict[key]
+                if input_param.shape != param.shape:
+                    # local shape should match the one in checkpoint
+                    error_msgs.append('size mismatch for {}: copying a param of {} from checkpoint, '
+                                      'where the shape is {} in current model.'
+                                      .format(key, param.shape, input_param.shape))
+                    continue
+                if isinstance(input_param, Parameter):
+                    # backwards compatibility for serialized parameters
+                    input_param = input_param.data
+                try:
+                    param.copy_(input_param)
+                except Exception:
+                    error_msgs.append('While copying the parameter named "{}", '
+                                      'whose dimensions in the model are {} and '
+                                      'whose dimensions in the checkpoint are {}.'
+                                      .format(key, param.size(), input_param.size()))
+            elif strict:
+                missing_keys.append(key)
+
+    def forward(self, input):
+        self._check_input_dim(input)
+
+        y_s = self.bias / (self.weight.abs() + self.eps)
+        y_t = self.bias_target.data / (self.weight_target.data.abs() + self.eps)
+
+        y_s = y_s.view(-1, 1)
+        y_t = y_t.view(-1, 1)
+
+        corr = -torch.abs(y_t - y_s.t())
+        corr = torch.softmax(corr/self.tem, dim=1)
+
+        # sparse C
+        sparse_corr = torch.where((corr - torch.diag(corr).unsqueeze(1)) >= 0, corr, torch.zeros_like(corr))
+
+        # recalibrating the channels in the target model
+        trans_weight = sparse_corr.detach() @ self.weight_target.view(self.num_features, 1)
+        trans_bias = sparse_corr.detach() @ self.bias_target.view(self.num_features, 1)
+        trans_weight = trans_weight.squeeze()
+        trans_bias = trans_bias.squeeze()
+
+
+        if self.training:
+
+            z_trans = F.batch_norm(
+                input, self.running_mean, self.running_var, trans_weight,
+                trans_bias,
+                True, self.momentum, self.eps)
+
+            z = F.batch_norm(
+                input, self.running_mean, self.running_var, self.weight_target, self.bias_target,
+                True, self.momentum, self.eps)
+
+        else:
+
+            z_trans = F.batch_norm(
+                input, self.running_mean, self.running_var, trans_weight,
+                trans_bias,
+               self.training or not self.track_running_stats, self.momentum, self.eps)
+
+            z = F.batch_norm(
+                input, self.running_mean, self.running_var, self.weight_target, self.bias_target,
+               self.training or not self.track_running_stats, self.momentum, self.eps)
+
+        return z + z_trans
+
+    def extra_repr(self):
+        return '{num_features}, eps={eps}, momentum={momentum}, affine={affine}, ' \
+               'track_running_stats={track_running_stats}'.format(**self.__dict__)
+
+    def _load_from_state_dict(self, state_dict, prefix, metadata, strict,
+                              missing_keys, unexpected_keys, error_msgs):
+        version = metadata.get('version', None)
+        if (version is None or version < 2) and self.track_running_stats:
+            # at version 2: added num_batches_tracked buffer
+            #               this should have a default value of 0
+            num_batches_tracked_key = prefix + 'num_batches_tracked'
+            if num_batches_tracked_key not in state_dict:
+                state_dict[num_batches_tracked_key] = torch.tensor(0, dtype=torch.long)
+
+        self._load_from_state_dict_from_pretrained_model(
+            state_dict, prefix, metadata, strict,
+            missing_keys, unexpected_keys, error_msgs)
+
+
+class AffineCoNorm3d(_AffineCoNorm):
+    r"""Applies Batch Normalization over a 5D input (a mini-batch of 3D inputs
+    with additional channel dimension) as described in the paper
+    `Batch Normalization: Accelerating Deep Network Training by Reducing Internal Covariate Shift`_ .
+    .. math::
+        y = \frac{x - \mathrm{E}[x]}{ \sqrt{\mathrm{Var}[x] + \epsilon}} * \gamma + \beta
+    The mean and standard-deviation are calculated per-dimension over
+    the mini-batches and :math:`\gamma` and :math:`\beta` are learnable parameter vectors
+    of size `C` (where `C` is the input size).
+    By default, during training this layer keeps running estimates of its
+    computed mean and variance, which are then used for normalization during
+    evaluation. The running estimates are kept with a default :attr:`momentum`
+    of 0.1.
+    If :attr:`track_running_stats` is set to ``False``, this layer then does not
+    keep running estimates, and batch statistics are instead used during
+    evaluation time as well.
+    .. note::
+        This :attr:`momentum` argument is different from one used in optimizer
+        classes and the conventional notion of momentum. Mathematically, the
+        update rule for running statistics here is
+        :math:`\hat{x}_\text{new} = (1 - \text{momentum}) \times \hat{x} + \text{momemtum} \times x_t`,
+        where :math:`\hat{x}` is the estimated statistic and :math:`x_t` is the
+        new observed value.
+    Because the Batch Normalization is done over the `C` dimension, computing statistics
+    on `(N, D, H, W)` slices, it's common terminology to call this Volumetric Batch Normalization
+    or Spatio-temporal Batch Normalization.
+    Args:
+        num_features: :math:`C` from an expected input of size
+            :math:`(N, C, D, H, W)`
+        eps: a value added to the denominator for numerical stability.
+            Default: 1e-5
+        momentum: the value used for the running_mean and running_var
+            computation. Can be set to ``None`` for cumulative moving average
+            (i.e. simple average). Default: 0.1
+        affine: a boolean value that when set to ``True``, this module has
+            learnable affine parameters. Default: ``True``
+        track_running_stats: a boolean value that when set to ``True``, this
+            module tracks the running mean and variance, and when set to ``False``,
+            this module does not track such statistics and always uses batch
+            statistics in both training and eval modes. Default: ``True``
+    Shape:
+        - Input: :math:`(N, C, D, H, W)`
+        - Output: :math:`(N, C, D, H, W)` (same shape as input)
+       .. _`Batch Normalization: Accelerating Deep Network Training by Reducing Internal Covariate Shift`:
+        https://arxiv.org/abs/1502.03167
+    """
+
+    def _check_input_dim(self, input):
+        if input.dim() != 5:
+            raise ValueError('expected 5D input (got {}D input)'
+                             .format(input.dim()))
+
---
+++ get_ac_corr.py
@@ -0,0 +1,79 @@
+import torch
+import numpy as np
+
+
+class AC_Corr(object):
+    """
+        Calculate the AC-Corr for transferability estimation.
+        source-info: {'model_path':'pretrained_model_path', prefix:'module.'}
+        target-info: {'model_path':'target_model_path_at_one_epoch', prefix:'module.'}
+        layer_key: At which layer to compute the AC-Corr
+    """
+
+    def __init__(self, source_info: dict, target_info: dict, layer_key):
+        self.source_model_path = source_info['model_path']
+        self.target_model_path = target_info['model_path']
+        self.source_prefix = source_info['prefix']
+        self.target_prefix = target_info['prefix']
+        self.layer_key = layer_key
+
+    def get_corr(self, weight, bias, weight_target, bias_target, eps=1e-5):
+        y_s = bias / (weight.abs() + eps)
+        y_t = bias_target.data / (weight_target.data.abs() + eps)
+        y_s = y_s.view(-1, 1)
+        y_t = y_t.view(-1, 1)
+        corr = -torch.abs(y_t - y_s.t())
+        corr = torch.softmax(corr, dim=1)
+        corr = torch.where((corr - torch.diag(corr).unsqueeze(1)) >= 0, corr, torch.zeros_like(corr))
+        corr = corr.cpu().numpy()
+        return corr
+
+    def get_state_dict(self, load_from, prefix='module.'):
+        # load a pretrained model
+        checkpoint = torch.load(load_from)['state_dict']
+        print('Load model from:', load_from)
+
+        # remove prefix
+        state_dict = {k[len(prefix):]: v for k, v in checkpoint.items()}
+
+        # select the transferred layer
+        layer_names = []
+        for (k, v) in state_dict.items():
+            for layer in self.layer_key:
+                if layer in k:
+                    #if 'up_conv' not in k:
+                    start = len(layer) #+ 7
+                    if k[:start] not in layer_names:
+                        layer_names.append(k[:start])
+
+        return state_dict, layer_names
+
+    def cal_ac_corr(self):
+        source_dict, source_conv_names = self.get_state_dict(self.source_model_path, self.source_prefix)
+        target_dict, target_conv_names = self.get_state_dict(self.target_model_path, self.target_prefix)
+
+        # get each bn layer
+        corr_sum = []
+        for layer in source_conv_names:
+            bn_layer_name = layer + 'bn1'
+            print('Layer:', bn_layer_name)
+            weight = source_dict[bn_layer_name + '.weight']
+            bias = source_dict[bn_layer_name + '.bias']
+            weight_target = target_dict[bn_layer_name + '.weight_target']
+            bias_target = target_dict[bn_layer_name + '.bias_target']
+            corr = self.get_corr(weight, bias, weight_target, bias_target, eps=1e-5)
+            corr_sum.append(corr.sum())
+
+        ac_corr = np.mean(corr_sum)
+        print('AC-Corr:', ac_corr)
+        return ac_corr
+
+
+if __name__ == '__main__':
+    source_info = {'model_path': 'the path to the pretrained model',
+     'prefix': 'module.'}
+    target_info = {'model_path': 'the path to the target model which is fine-tuned with AC-Norm for only one    epoch',
+    'prefix': 'module.'}
+    AC_Corr = AC_Corr(source_info, target_info, layer_key=['down_tr512.ops.1.'])
+    Trans_value = AC_Corr.cal_ac_corr()
+
